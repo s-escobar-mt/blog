454c27a1c8cc33210be0267bb73ed4a014efdbcd
Sydney Escobar <se176171@umconnect.umt.edu>
2024-10-08 01:49:23 -0600

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin): # new
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+    
+admin.site.register(Post, PostAdmin) # new
